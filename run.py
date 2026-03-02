45b976c91d45095ce3fd73c6eeb97d162a38bf39
nguyenvanhieu1710 <hieunghiendo1@gmail.com>
2026-03-02 11:19:33 +0700

    fix: update port

--- run.py
+++ run.py
@@ -1,4 +1,9 @@
+import os
 import uvicorn
+from dotenv import load_dotenv
+load_dotenv()
+
+port = int(os.environ.get("PORT", 8000))
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=port, reload=True)
